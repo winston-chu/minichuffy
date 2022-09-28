cff5217960122ea8ae8f5a529c5e6f35171e3b44
Winston Chu <winstonchu@Justins-MBP.lan>
2022-09-28 18:28:27 -0400

    Revised Code

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,8 @@ from discord.ext import commands
 
 TOKEN = "ODc3NjU0ODEzMDkxOTU0NzE4.Grr4Mk.1vreJHRndUmar0qoIUkekKbEwmc67Af8-DFPeo"
 
-client = commands.Bot(command_prefix="-")
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="-", intents=intents)
 # client = discord.Client()
 # bot = commands.Bot(command_prefix=".")
 
@@ -27,54 +28,73 @@ async def on_message(message):
 
     if message.author == client.user:
         return
+
     if user_message.lower() == "hi":
         await message.channel.send(f"ok")
-        return
-    elif user_message.lower() == "bye":
+
+    if user_message.lower() == "bye":
         await message.channel.send(f"xD")
-        return
-    elif user_message.lower() == "ok":
+
+    if user_message.lower() == "ok":
         await message.channel.send(f"hi")
-        return
-    elif "i just" in user_message.lower():
+
+    if "i just" in user_message.lower():
         await message.channel.send(f"first time?")
-        return
-    elif "<:OMEGALUL:775515868410544149>" in user_message:
-        await message.reply(f"SHEESH!", mention_author = False)
-    elif "donowall" in user_message.lower():
-        await message.reply(f"I should've voted you for meanest person", mention_author = False)
-    elif "four" in user_message.lower():
+
+    if "<:OMEGALUL:775515868410544149>" in user_message:
+        await message.reply(f"SHEESH!", mention_author=False)
+
+    if "donowall" in user_message.lower():
+        await message.reply(f"I should've voted you for meanest person", mention_author=False)
+
+    if "four" in user_message.lower():
         await message.channel.send(f"fo?")
-    elif "fo?" in user_message.lower():
-        await message.channel.send(f"https://imgur.com/a/IQpgMU9")
-    elif "devour" in user_message.lower():
-        await message.reply(f"https://tenor.com/view/lottery-loser-rat-mouse-gif-12761681", mention_author = False)
-    elif "val" in user_message.lower():
-        await message.reply(f"https://cdn.discordapp.com/attachments/770043097019056148/916065524172071002/D7_NFf60C-0Nd11gyqf4ulUcRZ3vATfsdkZCI5Pe6Gwiz6MB7-sObSF7H3mKGXUbwwr4Ehj7t8Urj2Ms765-nd-v1.png", mention_author = False)
-    elif "ganyu" in user_message.lower() and user_message != (f"https://tenor.com/view/ganyu-flowers-eat-eating-ganyu-poggers-gif-23827527"):
-        await message.reply(f"https://tenor.com/view/gigachad-genshin-keqing-gif-23205874", mention_author = False)
-    elif "i am" in user_message.lower():
+
+    if "fo?" in user_message.lower():
+        await message.channel.send(f"https://i.imgur.com/VeTNSCg.jpeg")
+
+    if "devour" in user_message.lower():
+        await message.reply(f"https://tenor.com/view/lottery-loser-rat-mouse-gif-12761681", mention_author=False)
+
+    if "val" in user_message.lower():
+        await message.reply(f"https://cdn.discordapp.com/attachments/770043097019056148/916065524172071002/D7_NFf60C-0Nd11gyqf4ulUcRZ3vATfsdkZCI5Pe6Gwiz6MB7-sObSF7H3mKGXUbwwr4Ehj7t8Urj2Ms765-nd-v1.png", mention_author=False)
+
+    if "ganyu" in user_message.lower() and user_message != (f"https://tenor.com/view/ganyu-flowers-eat-eating-ganyu-poggers-gif-23827527"):
+        await message.reply(f"https://tenor.com/view/gigachad-genshin-keqing-gif-23205874", mention_author=False)
+
+    if "i am" in user_message.lower():
         name = lMessage.split("i am", 1)[1]
         await message.channel.send(f"hi" + name)
-    elif "<@!746078147463741590>" in user_message:
+
+    if "<@!746078147463741590>" in user_message:
         if user_message == "<@!746078147463741590>":
             await message.channel.send(username + ": Michael")
             await message.channel.send(f"https://tenor.com/view/markiplier-funny-meme-what-gif-22548813")
         else:
             michaelMessage = user_message.replace("<@!746078147463741590>", "")
             await message.channel.send(username + ":" + michaelMessage)
-        # await message.delete()
-    elif "on god" in user_message.lower():
-        await message.reply(f"On fracture!", mention_author = False)
-    elif "arcane" in user_message.lower():
+        # await message.delete
+
+    if message.author.id == 366269406214619136:
+        emoji = "🤓"
+        await message.add_reaction(emoji)
+
+    if message.author.id == 321027153489559553 and "I " == user_message[0:2].upper():
+        emoji = "👑"
+        await message.add_reaction(emoji)
+
+    if "on god" in user_message.lower():
+        await message.reply(f"On fracture!", mention_author=False)
+
+    if "arcane" in user_message.lower():
         await message.channel.send(f"https://tenor.com/view/space-launch-funny-meme-space-force-space-gif-17548362")
 
-        if "Recent osu! Standard Play for" in user_message:
-            rand = random.randint(1, 3)
-            if rand == 1:
-                await message.channel.send(f"WTF, you're insane????")
-        elif "choke" in user_message.lower():
-            await message.channel.send(f"https://tenor.com/view/gene-issue-gif-22947786")
+    if "Recent osu! Standard Play for" in user_message:
+        rand = random.randint(1, 3)
+        if rand == 1:
+            await message.channel.send(f"WTF, you're insane????")
+    if "choke" in user_message.lower():
+        await message.channel.send(f"https://tenor.com/view/gene-issue-gif-22947786")
 
     if message.channel.name == "the-mgg-channel-and-no-mic":
         if "thank you" in user_message.lower() or "thanks" in user_message.lower():
